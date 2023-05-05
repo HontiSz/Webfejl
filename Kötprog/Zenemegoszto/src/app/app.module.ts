96c15c2eae83df5766631b42915b509e889c44f7
Honti Szabolcs <h045923@stud.u-szeged.hu>
2023-05-05 14:12:28 +0200

    second sht

--- Kötprog/Zenemegoszto/src/app/app.module.ts
+++ Kötprog/Zenemegoszto/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './pages/list/list.component';
+import { UploadComponent } from './pages/upload/upload.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    UploadComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Kötprog/Zenemegoszto/src/environments/environment.prod.ts
+++ Kötprog/Zenemegoszto/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'zenemegoszto-533d3',
+    appId: '1:9137806834:web:7e62b5e6306bc7ac989f6b',
+    storageBucket: 'zenemegoszto-533d3.appspot.com',
+    locationId: 'europe-west3',
+    apiKey: 'AIzaSyCpAA1jlPiXw_DS-l26W7FJHU8WQbYZ_Hw',
+    authDomain: 'zenemegoszto-533d3.firebaseapp.com',
+    messagingSenderId: '9137806834',
+  },
   production: true
 };
